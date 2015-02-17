669a3b39d323ec455a15630f8df1636265e49d58
= <=>
2015-02-17 13:35:38 -0600

    Added readme file

--- download-after-terms.php
+++ download-after-terms.php
@@ -1,15 +1,12 @@
 <?php
 /**
- * Plugin Name: Terms After Download
- * Plugin URI: http://github.com/alexesba/dowload-after-download/
- * Description: Shows a popup dialog with terms and conditions
+ * Plugin Name: Download Affter Agree
+ * Plugin URI: http://github.com/alexesba/download-after-agree
+ * Description: Insert a checkbox to accept or shows a popup dialog with terms and conditions
  * Version: 1.0.0
  * Author: Alejandro Espinoza
  * Author URI: http://github.com/alexesba
- * License: GPL2
  */
-
-
 // Register the shortcodes
 // add_shortcode('dat_link', 'shortcode_handler_dat_link');
 add_shortcode('dat_terms', 'shortcode_handler_dat_terms');
@@ -18,29 +15,38 @@ add_shortcode('dat_terms', 'shortcode_handler_dat_terms');
 // Shortcode handler function: inserts a div with the EULA agreement content
 //
 // Allowed properties:
-// -  eula_page_id     ->    ID of the EULA page displayed in the dialog [required]
-// -  modalbox_title    ->    The title of the dialog to be displayed [optional]
-// -  class             ->    CSS class of the div enclosing the dialog content [optional]
-// -  padding           ->    Padding between the dialog frame and the inner content [optional]
-// -  width             ->    Width of the dialog [optional]
-// -  accept_button_text    ->    Text for the OK button [optional]
+// -  eula_page_id        -> ID of the EULA page displayed in the dialog [required]
+// -  modalbox_title      -> The title of the dialog to be displayed [optional]
+// -  class               -> CSS class of the div enclosing the dialog content [optional]
+// -  padding             -> Padding between the dialog frame and the inner content [optional]
+// -  width               -> Width of the dialog [optional]
+// -  agree_button_text   -> Text for the OK button [optional]
+// -  eula_link_text      -> Text to be placed before the link to the terms and  conditions page
+// -  eula_link_url_text  -> Text to be  wrapped between the link to the terms and condition page
+// -  alert_agree_message -> Text for the alert message when the user doesn't  accept the terms using the checkbox
+// -  modal               -> boolean value(0|1) to display the EULA in a modal box.
 //
+
 function shortcode_handler_dat_terms($atts)
 {
    // Set up attribute defaults where nothing has been specified
    extract(shortcode_atts(array(
-         'modalbox_title'    => 'Terms and Conditions',
-         'class'           => 'entry',
-         'padding'         => '20px',
-         'width'           => '80%',
-         'agree_button_text'  => 'I agree to the terms',
-         'eula_page_id'   => 0,
-         'modal' => 1
+         'modalbox_title'      => 'Terms and Conditions',
+         'class'               => 'entry',
+         'padding'             => '20px',
+         'width'               => '80%',
+         'agree_button_text'   => 'I agree to the terms',
+         'eula_link_url_text'  => 'terms & conditions',
+         'eula_ink_text'       => 'I agree to the',
+         'alert_agree_message' => 'Please aggre with the terms and conditions',
+         'eula_page_id'        => 0,
+         'modal'               => 0
       ), $atts));
 
    // Get the libraries we need
     wp_enqueue_script('jquery');
-   if($modal){
+
+   if($modal){#included if the modalbox is required
      wp_enqueue_script('jquery-ui-dialog');
      wp_enqueue_style('wp-jquery-ui-dialog');
    }
@@ -48,17 +54,22 @@ function shortcode_handler_dat_terms($atts)
    // Get the terms page
    if (empty($eula_page_id))
       return "";
-   else
-      $terms_page = get_post($eula_page_id, "OBJECT", "display");
-
-   // Get the terms page content
-   $terms_page_content = $terms_page->post_content;
-   // Convert double line breaks into paragraphs, replacing \n with <br /> to the string
-   $terms_page_content = wpautop($terms_page_content);
-   // Remove non-printable characters
-   $terms_page_content = preg_replace('/[\x00-\x1F]/u', '', $terms_page_content);
-   // HTML-encode double quotes because the string will be enclosed in double quotes
-   $terms_page_content = str_replace ('"', "&quot;", $terms_page_content);
+   else{
+     if($modal) {#parse the content of the EULA page only if modal is required
+       $terms_page = get_post($eula_page_id, "OBJECT", "display");
+       // Get the terms page content
+       $terms_page_content = $terms_page->post_content;
+       // Convert double line breaks into paragraphs, replacing \n with <br /> to the string
+       $terms_page_content = wpautop($terms_page_content);
+       // Remove non-printable characters
+       $terms_page_content = preg_replace('/[\x00-\x1F]/u', '', $terms_page_content);
+       // HTML-encode double quotes because the string will be enclosed in double quotes
+       $terms_page_content = str_replace ('"', "&quot;", $terms_page_content);
+     }else{
+       //Get the url of the EULA page
+       $terms_page_url = get_permalink($eula_page_id);
+     }
+   }
 
    // Build the output string
    $output = <<<EndOfHeredoc
@@ -72,20 +83,27 @@ function shortcode_handler_dat_terms($atts)
    </script>
 
    <script type="text/javascript">
-
+      //Remove the attribute href and insert a data url attribute with the same value
       var convertHrefToData = function(elements){
          elements.data('url', elements.prop('href'));
          elements.removeAttr("href");
       }
-
+      // Function to insert the checkbox using jquery
       var InsertChekbox = function(element){
         var checbox = jQuery('<input>', { type: 'checkbox', name: 'agree_eula' });
-        var label = jQuery('<label>', { text: 'I agree with the terms and conditions', for: 'agree_eula'});
-        element.append(label);
+        var label = jQuery('<label>', { text: ' {$eula_ink_text} ', for: 'agree_eula'});
+        var link = "<a class='dat_link' href='{$terms_page_url}' target='_blank'>{$eula_link_url_text}</a>";
+        label.append(link);
         element.append(checbox);
+        element.append(label);
       }
 
-      var showModalBox = function(height, url){
+      var downloadFile = function(url){
+          window.location.href = url;
+      }
+
+      var showModalBox = function(url){
+          var height = jQuery(window).height() * 0.8;
           jQuery("#dat_terms").dialog(
             {
                dialogClass: 'wp-dialog',
@@ -99,7 +117,7 @@ function shortcode_handler_dat_terms($atts)
                   "{$agree_button_text}": function()
                   {
                      jQuery(this).dialog("close");
-                     window.location.href = url;
+                     downloadFile(url);
                   },
                   Cancel: function()
                   {
@@ -117,23 +135,20 @@ function shortcode_handler_dat_terms($atts)
       }
       jQuery(document).ready(function ($)
       {
-         // Show the dialog
+         // Show the modalbox
          var downloadAgree = $('.agree_download a');
          convertHrefToData(downloadAgree);
          if(!$modal) InsertChekbox(downloadAgree.closest('div'));
          downloadAgree.click(function (e)
          {
-            e.preventDefault();
-            console.log('Display Dialog');
             var url = $(this).data('url');
-            var height = $(window).height() * 0.8;
             if($modal){
-              showModalBox(height, url);
+              showModalBox(url);
             }else{
               if($('input[name="agree_eula"]').is(':checked')){
-                window.location.href = url;
+                  downloadFile(url);
                 }else{
-                 alert('Please accept the terms and conditions before donwnlad');
+                 alert('{$alert_agree_message}');
                 }
             }
             e.preventDefault();
---
+++ readme.md
@@ -0,0 +1,53 @@
+﻿=== Download After Download===
+Contributors: alexesba
+Tags: download,terms,eula,license
+Requires at least: 3.5
+Tested up to: 4.0
+Stable tag: trunk
+License: GPL2
+
+Shows a popup dialog with terms and conditions (EULA) that must be accepted before a file can be downloaded
+
+== Description ==
+Terms Before Download adds a shortcode that can be used instead of HTML anchors to link to downloadable files. If such a link is clicked a popup dialog shows terms and conditions (EULA) which must be accepted for the download to start.
+
+The terms and conditions are read from a Wordpress page. That way there is only a single place to maintain the terms and they can easily be displayed independently of the plugin.
+
+The plugin supports Google Analytics to keep track of the number of downloads.
+
+An example of the plugin in action can be found here: http://helgeklein.com/download/
+
+#Usage
+
+[dat_terms  eula_page_id=20 ok_button_text='Custom agree text' dialog_title='Custom agree modalbox title' eula_ink_text='I agree with the' eula_link_url_text='terms and conditions' modal='0']
+
+
+# Parameters
+
+The following parameters can be used with the shortcode *dat_terms*:
+
+##Properties:
+*  eula_page_id        -> ID of the EULA page displayed in the dialog [required]
+*  modalbox_title      -> The title of the dialog to be displayed [optional if the modal value is not set]
+*  class               -> CSS class of the div enclosing the dialog content [optional]
+*  padding             -> Padding between the dialog frame and the inner content [optional]
+*  width               -> Width of the dialog [optional]
+*  agree_button_text   -> Text for the agree button [optional]
+*  eula_link_text      -> Text to be placed before the link to the terms and  conditions page
+*  eula_link_url_text  -> Text to be  wrapped between the link to the terms and condition page
+*  alert_agree_message -> Text for the alert message when the user doesn't  accept the terms using the checkbox
+*  modal               -> boolean value(0|1) to display the EULA in a modal box.
+
+##Default Values
+```
+         'modalbox_title'      => 'Terms and Conditions'
+         'class'               => 'entry'
+         'padding'             => '20px'
+         'width'               => '80%'
+         'agree_button_text'   => 'I agree to the terms'
+         'eula_link_url_text'  => 'terms & conditions'
+         'eula_ink_text'       => 'I agree to the'
+         'alert_agree_message' => 'Please aggre with the terms and conditions'
+         'eula_page_id'        => 0
+         'modal'               => 0
+```
